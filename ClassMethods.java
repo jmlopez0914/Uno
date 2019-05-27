69b5a59021193424dd51e4a315dbf0c58c3d9427
WightUnderscore <39373446+WightUnderscore@users.noreply.github.com>
2019-05-27 12:35:15 -0500

    File For Listing Public Class Methods And Progress

--- ClassMethods.java
+++ ClassMethods.java
@@ -13,7 +13,7 @@ Public Methods, Constructors and Constants For Each Class:
      - void render(Graphics g, int x, int y) ❌ 
   - Deck:
      - Deck() ❌ The deck should shuffle() after adding all the cards
-     - static final Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW}; ❌ Make this public please.
+     - static final Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW}; ❌ Move to Card and make public please.
      - void shuffle() ✅
      - void add(Card c) ✅ seems a little inefficient, but whatever
      - Card draw() ✅ Only Game should be calling this!
@@ -26,34 +26,37 @@ Public Methods, Constructors and Constants For Each Class:
         - void add(Card drawnCard) ❌ Think about it like poker; you ask the dealer to give you a card rather than grab it yourself
         - Card remove(int index) ❌ The equivalent of playing the card by giving it to the dealer
         - boolean getVisibility() ❌ 
-        - void setVisibility(boolean v) ❌ As you should realize, a new private variable goes with this
+        - void setVisibility(boolean v) ❌ As you'll realize, a new private variable goes with these two
         - ArrayList<Integer> getUsefulIndices(Card topCard) ❌ I figured this would help more since remove(int)
         - void render(Graphics g, int x, int y) ❌ the visibility determines whether to show faces or backs
      - Stub Classes in Hand:
         - boolean getUnoCall() ❌ This function returns whether a player said "Uno!" since their last turn.
         - int findPlay(Card topCard) ❌ Likely starts by calling getUsefulIndices(topCard)
+	- boolean playDrawnCard() ❌ Similar to findPlay(), but only in consideration to the drawn card
         - Color selectColor() ❌ 
         - boolean challengeDrawFour(>some parameters may help with decision making<) ❌
      - PlayerHand:
         - PlayerHand() ❌ This hand should start visible
         - boolean getUnoCall() ❌ All these will need to communicate with the GUI in some way
         - int findPlay(Card topCard) ❌ 
+	- boolean playDrawnCard() ❌
         - Color selectColor() ❌ I imagine a pop-up box with this
         - boolean challengeDrawFour(>I'll give parameters even if it doesn't matter<) ❌
      - ComputerHand:
         - ComputerHand() ❌ This hand should start not visible
         - boolean getUnoCall() ❌ RNG?
         - int findPlay(Card topCard) ❌ Your findPlay() was a nice start :)
+	- boolean playDrawnCard() ❌ This probably just calls isUseful() on the new Card
         - Color selectColor() ❌ Whatever the computer has the most of, I'd assume
         - boolean challengeDrawFour(>some parameters may help with decision making<) ❌
   - Game:
      - Game(int players) ✅ 
      - static void main(String args[]) ❌ Does not loop the game's events (a few private functions for game logic are incomplete)
   - UnoGUI:
-     - UnoGUI() ❌ if UnoGUI extends JFrame, then super("Title"); this also makes paint(g) easier to use
+     - UnoGUI() ❌ if UnoGUI extends JFrame, then super("Title"); this will also make paint(g) easier to use
      - static final double CARD_CONVERSION_FACTOR = 3/2; ❌ This stuff helps with the Card's and Hand's render() functions
-     - static final int CARD_HEIGHT = (int) 68 * CARD_CONVERSION_FACTOR; ❌
-	   - static final int CARD_WIDTH = (int) 44 * CARD_CONVERSION_FACTOR; ❌
+     - static final int CARD_HEIGHT = (int) 68 * CARD_CONVERSION_FACTOR; ❌ Possibly also move these three to Card
+     - static final int CARD_WIDTH = (int) 44 * CARD_CONVERSION_FACTOR; ❌ All my images are 44x68, if you're wondering
      - void paint(Graphics g) ❌ Takes over paintComponent(). Start with super.paint(g);
      - void takeCard(Card card, Hand hand) ❌ If you plan on cutting out animations, you may need need neither of these functions
      - void placeCard(Card card, Hand hand) ❌
